45b3d3b6a66c7bc0d7bb82a9feac35c5028dd2ad
Taswoor Hussain <taswoorhossein72@gmail.com>
2026-02-02 17:49:13 +0500

    configure mongoose db

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+import chalk from 'chalk';
 import app from './app.js';
-
+import ConnectDB from './config/db.config.js';
 const PORT = process.env.PORT || 7070;
 
-// listen 
-app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`);
+ConnectDB()
+.then(()=>{
+    app.listen(PORT , ()=>{
+        console.log(chalk.green.bgBlack.bold.underline("Server is running on port " , PORT))
+    })
+})
+.catch((err)=>{
+    console.log(chalk.red("Failed to run database and server due to ") )
 })
